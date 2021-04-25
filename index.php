5cc884987cc3105e66152dfcc6f3d7cafa18644e
noah mohabbat <noahrmohabbat@gmail.com>
2021-04-24 19:23:39 -0500

    php is owkring make sure things are installed, and are running localhost, also name file to be php file extension and not html

--- index.php
+++ index.php
@@ -3,7 +3,13 @@
         <b>Delivery Service</b>
         <link rel="stylesheet" href="index.css">
     </head>
+      <?php
+        echo "wow so cool"; 
+      ?>
     <body class="bg">
+      <?php
+        echo "wow so cool"; 
+      ?>
       <div align="right">
       Username:
       <input id = "username" type="text"/>
@@ -11,5 +17,11 @@
       Password:
       <input id = "password" type="text"/>
       </div>
+      <?php
+        echo "wow so cool"; 
+      ?>
     </body>
+      <?php
+        echo "wow so cool"; 
+      ?>
 </html>
